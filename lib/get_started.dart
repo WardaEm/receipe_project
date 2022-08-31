a56e6afdb5ada9ee54c84971d3a7e75c07021b31
Warda Emad <wardaemad32@std.mans.edu.eg>
2022-08-31 23:14:10 +0200

    first commit

---
+++ lib/get_started.dart
@@ -0,0 +1,77 @@
+
+
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+
+
+
+class GetStarted extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height ,
+                  decoration: BoxDecoration(
+                    color:Colors.deepOrange,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(50),
+                      topLeft: Radius.circular(50),
+                      bottomLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      SizedBox(height: 65),
+                      Text(
+                        'Hello! This is a receipt menu',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w600,
+                            letterSpacing: 1.2),
+                      ),
+
+                        Image.asset('images/png.png',
+
+                          height: 500,
+                        ),
+
+
+
+
+
+                SizedBox(height: 90,),
+                Container(child: TextButton(onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>Home()));
+                }, child: Text('Get Started',style: TextStyle(color: Colors.black,fontSize: 30,fontWeight: FontWeight.bold),)))]
+
+                  )
+                ) ]
+                )
+
+            // SizedBox(height: 50,),
+            // Container(child: TextButton(onPressed: (){
+            //   Navigator.push(context, MaterialPageRoute(builder: (context)=>Home()));
+            // }, child: Text('Get Started',style: TextStyle(color: Colors.deepOrange,fontSize: 30,fontWeight: FontWeight.bold),)))
+
+          ]
+
+        ),
+
+      ),
+    );
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('dfdrr')
+          ],
+        ),
+      ),
+    );
+  }
+}
